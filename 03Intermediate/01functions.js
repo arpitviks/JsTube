d9f86a2ef5916814d05b491664bf6d65442c8398
Arpit Vikhar <arpitvikhar@gmail.com>
2022-04-25 22:15:00 +0530

    4th Commit

--- 03Intermediate/01functions.js
+++ 03Intermediate/01functions.js
@@ -1,1 +1,13 @@
-dfdfadfd
+function sayHello(name) {
+    console.log(`Hello there, ${name}, How are you ?`)
+}
+
+sayHello('Arpit');
+
+function namastey() {
+    return "Hello in India";
+}
+
+var greetings = namastey();
+
+console.log(greetings);

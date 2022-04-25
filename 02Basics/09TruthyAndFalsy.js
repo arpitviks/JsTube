b8ba80561f43282951010e6a4c637a9ac5a8ecc4
Arpit Vikhar <arpitvikhar@gmail.com>
2022-04-25 21:43:06 +0530

    second commit

---
+++ 02Basics/09TruthyAndFalsy.js
@@ -0,0 +1,13 @@
+var user = 2;
+var usr = '';
+
+if (2 == user) {
+    console.log('Without strict type checking');
+}
+
+if (usr) {
+    console.log('this is false');
+}
+if (2 === user) {
+    console.log('With strict type checking');
+}

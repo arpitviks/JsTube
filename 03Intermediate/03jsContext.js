ed1f163e07bc560834e3934d99e4e2f70b1862fd
Arpit Vikhar <arpitvikhar@gmail.com>
2022-04-27 10:36:01 +0530

    new file

---
+++ 03Intermediate/03jsContext.js
@@ -0,0 +1,13 @@
+sayHello();
+
+function sayHello() {
+    console.log("Hello");
+}
+
+var myName = "hitesh";
+if (myName === window.myName) {
+    console.log("This is again a true statement");
+}
+
+//In js there's a global context in which all the fuctions are stored and 
+//then these fuctions are accessed based on the 

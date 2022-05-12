3cdda462d0ccb22248258ce9c7bab5da27ad1385
Arpit Vikhar <arpitvikhar@gmail.com>
2022-05-12 23:00:41 +0530

    this keyword

--- 03Intermediate/15thisAgain.js
+++ 03Intermediate/15thisAgain.js
@@ -5,13 +5,16 @@ var user = {
     firstname: "Arpit",
     courseCount: 4,
     getCourseCount: function () {
-        console.log("LINE No 8", this);
+        console.log("LINE No 8", this);         //It will return the context as a user object as getCourseCount() is getting called from the "user" object
         function sayHello() {
             console.log("Hello");
-            console.log("Line 30", this);
+            console.log("Line 30", this);       //It will return the context as a "Windows" because it is gettng called independently not with the help of any object.
         }
         sayHello();
     }
 }
 
 user.getCourseCount();
+
+//Execute this code in chrome console
+
